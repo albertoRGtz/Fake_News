d45158022ef1bc794d00b36b240bb27776ca9e4c
diegobalam <85011366+diegobalam@users.noreply.github.com>
2021-10-04 20:03:11 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='gym_fake',
+      version='0.0.1',
+      install_requires=['gym','pandas','numpy','selenium']  # And any other dependencies foo needs
+      )

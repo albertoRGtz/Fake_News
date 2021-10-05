d80645ee18cb11961ae28f7a335c5ea1e1124d10
diegobalam <85011366+diegobalam@users.noreply.github.com>
2021-10-05 17:16:55 -0500

    Update fake_env.py

--- gym_fake/envs/fake_env.py
+++ gym_fake/envs/fake_env.py
@@ -30,9 +30,9 @@ class FakeEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-    self.actions_grid=['query_return','query_query_step','record_return','record_step','stop']
+    self.actions_grid=['same_query','next_query','record_return','record_step','stop']
     self.actions_kb=["no_relation","positive","negative"]
-    self.noticias= pd.read_csv("/home/balam/balam/Fake_News/gym_fake/data/data.csv").drop_duplicates() ##base de noticias
+    self.noticias= pd.read_csv("/home/diegobalam/Fake_news/Fake_News-main/gym_fake/data/data.csv").drop_duplicates() ##base de noticias
     self.num_noticias=len(self.noticias)-1
     #self.web_data_frame = pd.DataFrame()
     self.noticias_ind=-1
@@ -63,10 +63,10 @@ class FakeEnv(gym.Env):
     self.model = AutoModel.from_pretrained(self.MODEL_NAME,output_hidden_states=True)
     self.tokenizer = AutoTokenizer.from_pretrained(self.MODEL_NAME)
     self.nlp1= pipeline('ner',model='vblagoje/bert-english-uncased-finetuned-pos', grouped_entities=True)
-
+    self.summarizer = pipeline("summarization")
     self.options = webdriver.FirefoxOptions()
     self.options.add_argument('-headless')
-    self.driver = webdriver.Firefox(executable_path=r'/home/balam/balam/Fake_News/gym_fake/envs/geckodriver', firefox_options=self.options)
+    self.driver = webdriver.Firefox(executable_path=r'/home/diegobalam/Fake_news/Fake_News-main/gym_fake/envs/geckodriver', firefox_options=self.options)
     
 
     #Space variables
@@ -128,9 +128,10 @@ class FakeEnv(gym.Env):
     """ debemos regresar un arreglo del tipo:
     respuesta:["estado","reward","booleando(si he termiando o no)"]"""
     self.num_steps_per_episode+=1
+    texto1=self.noticias['Headline'][self.noticias_ind]
     action_grid,action_kb=self._expandAction(action)
     query=self._action_grid_selector(action_grid)
-    reward=self._get_reward()
+    reward=self._get_reward(texto1,query,action_kb)
     self.query_seen[self.query_indexes[self.query_ind]]+=1
     query_seen=self.query_seen[self.query_indexes[self.query_ind]]
     stat=[]
@@ -157,18 +158,20 @@ class FakeEnv(gym.Env):
     self.hist_real=[]
 
      #set stepepisodeDFDF
-
-    self.episodeDF = pd.DataFrame.from_dict(duckduck_(self.noticias['Headline'][self.noticias_ind]))
+    Head=self.noticias['Headline'][self.noticias_ind]
+    self.episodeDF= pd.DataFrame.from_dict(self.duckduck_(self.noticias['Headline'][self.noticias_ind]))
     
+    self.realidad[0]=self.noticias['Label'][self.noticias_ind]
     #set Query lists and indexes for queryDFs
-    self.querys_step=list(set(self.episodeDF.search.tolist())) #Los querys
-    self.query_indexes=list(range(len(self.querys_step))) #Los índices de la lista de querys
+    self.querys_step=[Head,Head+'True',Head+'False'] #Los querys, se debe cambiar de momento no se necesita
+    self.query_indexes=[0,1,2] #Los índices de la lista de querys, depende de como se haga el paso anterior
     self.row_index_query=[0]*len(self.querys_step) #En qué registro está cada query
     self.query_seen=[0]*len(self.querys_step) #Cuantos pasos he dado en ese query
     self.query_ind=0 #En qué query estoy
 
-    self.querydf=self.episodeDF.loc[self.episodeDF["search"] == self.querys_step[self.query_indexes[self.query_ind]]]
+    #self.querydf=self.episodeDF.loc[self.episodeDF["search"] == self.querys_step[self.query_indexes[self.query_ind]]]
     self.episode_continues=False
+    
 
     return self.render()
 
@@ -177,7 +180,7 @@ class FakeEnv(gym.Env):
     #return self.querydf.iloc[self.row_index_query[self.query_indexes[self.query_ind]]].tolist(),self.agent_db
     query_seen=self.query_seen[self.query_indexes[self.query_ind]]
     #return [(query,self.num_steps_per_episode,query_seen,agent_db),reward,self.episode_continues]
-    return [np.array([self.num_steps_per_episode,query_seen],dtype=np.float64)]
+    return np.array([0]*1538,dtype=np.float64)
 
   def _action_grid_selector(self,action_grid):
       print(action_grid)
@@ -195,8 +198,9 @@ class FakeEnv(gym.Env):
         self._record_step()
       if action_grid==4:
       #stop is 4
-        return self.querydf.iloc[self.row_index_query[self.query_indexes[self.query_ind]]].tolist()
-      return self.querydf.iloc[self.row_index_query[self.query_indexes[self.query_ind]]].tolist()
+        return self.episodeDF['text'][self.row_index_query[self.query_indexes[self.query_ind]]]
+      return self.episodeDF['text'][self.row_index_query[self.query_indexes[self.query_ind]]]
+
 
     
 
@@ -208,21 +212,26 @@ class FakeEnv(gym.Env):
       self.query_ind=self.query_ind
     else:
       self.query_ind+=1
+    self.episodeDF=pd.DataFrame.from_dict(self.duckduck_(self.querys_step[self.query_ind]))
+
   def _query_return(self):
     if self.query_ind==0:
       self.query_ind=self.query_indexes[-1]
     else:
       self.query_ind-=1
+    self.episodeDF=pd.DataFrame.from_dict(self.duckduck_(self.querys_step[self.query_ind]))
+
+    
   def _record_return(self):
     
     if self.row_index_query[self.query_indexes[self.query_ind]]==0:
-      self.row_index_query[self.query_indexes[self.query_ind]]=len(self.querydf.index)-1
+      self.row_index_query[self.query_indexes[self.query_ind]]=len(self.episodeDF)-1
     else:
       self.row_index_query[self.query_indexes[self.query_ind]]-=1
 
   def _record_step(self):
     
-    if self.row_index_query[self.query_indexes[self.query_ind]]==len(self.querydf.index)-1:
+    if self.row_index_query[self.query_indexes[self.query_ind]]==len(self.episodeDF)-1:
       if self.query_ind==len(self.querys_step)-1:
         self.episode_continues=True
       else:
@@ -230,13 +239,16 @@ class FakeEnv(gym.Env):
     else:
       self.row_index_query[self.query_indexes[self.query_ind]]+=1
 
+    
+    
+  
 
 ####################################################################################################################################
 
   def tokeni(self,input):
     tokenized_text=[]
     for s in input['input_ids'].tolist():
-      a=tokenizer.convert_ids_to_tokens(s)
+      a=self.tokenizer.convert_ids_to_tokens(s)
       tokenized_text.append(a)
     return tokenized_text  
 
@@ -264,7 +276,7 @@ class FakeEnv(gym.Env):
         id=1
         lista.append(anterior)
         lista.append(token)
-        anterior=tokenizer.convert_tokens_to_string(lista)
+        anterior=self.tokenizer.convert_tokens_to_string(lista)
         tokenl.append(i-1)
         tokenl.append(i)
       else: 
@@ -308,7 +320,7 @@ class FakeEnv(gym.Env):
     return word_embedding
 
   def calculate_cos_distance(self,a,b):
-    cose= dot(a, b)/(norm(a)*norm(b))
+    cose= np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
     return 1 - cose
 
   def separar(self,texto):
@@ -326,9 +338,9 @@ class FakeEnv(gym.Env):
     return Noun,Verbs,Adj
 
   def trans(self,texto):
-    input = tokenizer(texto,return_tensors="pt")
-    tokens=tokeni(input)
-    output  = model(**input)
+    input = self.tokenizer(texto,return_tensors="pt")
+    tokens=self.tokeni(input)
+    output  = self.model(**input)
     hidden=output[2]
     X=hidden[-1].detach().numpy()
     X=X.squeeze()
@@ -344,44 +356,45 @@ class FakeEnv(gym.Env):
   def add_histre(self,dis2):
     self.hist_real.append(dis2)
   
-  def add_general(prom,prom1):
+  def add_general(self,prom,prom1):
     self.agent_db[0]=prom
     self.agent_db[1]=prom1
 
-  def stados(mat1,mat2):
+  def stados(self,mat1,mat2):
     self.state[0]=mat1
     self.state[1]=mat2
 
     
   def _get_reward(self,texto1,texto2, action):
-    mat1=trans(texto1)
-    mat2=trans(texto2)
-    dis=calculate_cos_distance(mat1[0][0],mat2[0][0])
-    stados(mat1[0][0],mat2[0][0])
+        
+    mat1=self.trans(texto1)
+    mat2=self.trans(texto2)
+    dis=self.calculate_cos_distance(mat1[0][0],mat2[0][0])
+    self.stados(mat1[0][0],mat2[0][0])
   
     if action==0:
       re=dis
     else:
       re=1-dis
   
-    res_ori = summarizer(texto1, max_length=25, min_length=5, do_sample=False)[0]['summary_text']
-    res_nuev = summarizer(texto2, max_length=25, min_length=5, do_sample=False)[0]['summary_text']
-    token_vecs_ori=trans(res_ori)
-    token_vecs_nuev=trans(res_nuev)
-    t_ori=token_esp(token_vecs_ori[1][0])
-    t_nuev=token_esp(token_vecs_nuev[1][0])
-    sep_ori=separar(res_ori)
-    sep_nuev=separar(res_nuev)
+    res_ori = self.summarizer(texto1, max_length=25, min_length=5, do_sample=False)[0]['summary_text']
+    res_nuev = self.summarizer(texto2, max_length=25, min_length=5, do_sample=False)[0]['summary_text']
+    token_vecs_ori=self.trans(res_ori)
+    token_vecs_nuev=self.trans(res_nuev)
+    t_ori=self.token_esp(token_vecs_ori[1][0])
+    t_nuev=self.token_esp(token_vecs_nuev[1][0])
+    sep_ori=self.separar(res_ori)
+    sep_nuev=self.separar(res_nuev)
   
     
     #Adjetivos:
-    Ad_ori=checar(sep_ori[1][0]['word'],token_vecs_ori[1][0])
-    Ad_nuev=checar(sep_nuev[1][0]['word'],token_vecs_nuev[1][0])
-    vec_ori=vec(Ad_ori[1],token_vecs_ori[0])
-    vec_nuev=vec(Ad_nuev[1],token_vecs_nuev[0])
+    Ad_ori=self.checar(sep_ori[1][0]['word'],token_vecs_ori[1][0])
+    Ad_nuev=self.checar(sep_nuev[1][0]['word'],token_vecs_nuev[1][0])
+    vec_ori=self.vec(Ad_ori[1],token_vecs_ori[0])
+    vec_nuev=self.vec(Ad_nuev[1],token_vecs_nuev[0])
     
   
-    dis2=calculate_cos_distance(vec_ori,vec_nuev)
+    dis2=self.calculate_cos_distance(vec_ori,vec_nuev)
     if action==1:
       re2=1-dis2
     
